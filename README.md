a88d6a1a919f8436b292dcc07c9aab0fa3a8dde9
Augusto Cesar <acs-03@hotmail.com>
2022-07-27 17:38:16 -0300

    Project stated

---
+++ README.md
@@ -0,0 +1,12 @@
+# DIO Fábrica de carros
+
+Este projeto foi desenvolvido no Bootcamp Santander Fullstack Developer pela <a href="https://www.dio.me/" target="_blank">DIO</a> no módulo Java, ele consiste em simular uma fábrica de carros utilizando alguns dos padrões de projeto mais utilizados da linguagem, abaixo uma lista das implementações:
+
+- Linguagem de programação: Java 8
+- IDE: Visual Studio Code
+
+## Implementações
+
+- Facade
+- Singleton
+- Strategy
---
+++ src/model/Car.java
@@ -0,0 +1,32 @@
+package model;
+
+/**
+ * Car
+ */
+public class Car {
+
+    public String model;
+    public String color;
+
+    public Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+}
